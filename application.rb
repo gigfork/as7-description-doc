46a08102b84210c510eac4ac4294c939e111e51e
Jeff Mesnil <jmesnil@gmail.com>
2012-07-24 12:26:33 +0200

    WIP

--- application.rb
+++ application.rb
@@ -2,7 +2,61 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require
 
+module TextFilter
+  def double(input)
+    input + input
+  end
+end
+
+def generate_toc(resource)
+  out = "<div id=toc>"
+  out += "<li><a href='#root'>root</a></li>"
+  if resource['children']
+    out += "<ul>"
+    resource['children'].each { |k, v| out += "<li><a href='##{k}'>#{k}</a></li>"}
+    out += "</ul>"
+  end
+  out += "</div>"
+  out.to_s
+end
+
+Liquid::Template.register_filter(TextFilter)
+
 get '/resource-description' do
+  user = params['user']
+  password = params['password']
+
+  return [500, "missing user and password parameters"] unless user and password
+
+  url = 'http://127.0.0.1:9990/management/'
+  operation = {:operation => "read-resource-description",
+      :recursive => params["recursive"] || false,
+      :operations => params["operations"] || false,
+      :inherited => params["inherited"] || false,
+      "json.pretty" => 1
+    }
+  res = AS7.query url, user, password, operation.to_json
+  if res.code != 200
+    [500, res.body.to_s]
+  end
+
+  result = JSON.parse(res.body.to_s)
+  if result['outcome'] != "success"
+    [500, res.body.to_s]
+   end
+   resource = result['result']
+   out = Liquid::Template.parse(File.read('./views/header.liquid')).render
+   out += generate_toc(resource)
+   out += Liquid::Template.parse(File.read('./views/index.liquid')).render 'resource' => resource
+   out += "<h2>Attributes</h2>"
+   resource['attributes'].each { |k, v | out += Liquid::Template.parse(File.read('./views/attribute.liquid')).render('name' => k, 'attribute' => v, 'base' => 'root')}
+   out += "<h2>Operations</h2>"
+   resource['operations'].each { |k, v | out += Liquid::Template.parse(File.read('./views/operation.liquid')).render('name' => k, 'operation' => v, 'base' => 'root')}
+   out += Liquid::Template.parse(File.read('./views/footer.liquid')).render
+   out
+end
+
+get '/resource-description2' do
   user = params['user']
   password = params['password']
 
